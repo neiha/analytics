631bfb4320f55236ff4ca012b3c8f94c52b24ae5
djorg06 <dj.jorg.ado@gmail.com>
2016-05-05 23:52:39 -0500

    Se agrega libreria para calendario

---
+++ analytics/assets/js/Reporte/Index.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    $('input[name="daterange"]').daterangepicker();
+    //function cb(start, end) {
+    //    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    //}
+    //cb(moment().subtract(29, 'days'), moment());
+
+    //$('#reportrange').daterangepicker({
+    //    ranges: {
+    //        'Today': [moment(), moment()],
+    //        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    //        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+    //        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+    //        'Last 90 Days': [moment().subtract(89, 'days'), moment()],
+    //        'Last 365 Days': [moment().subtract(364, 'days'), moment()]
+    //    }
+    //}, cb);
+
+});
